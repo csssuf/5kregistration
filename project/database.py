fa23a71256bf466d2591a600e5278975e8ea626a
James Forcier <jmf@csh.rit.edu>
2014-09-26 15:33:48 -0400

    update database URI

--- project/database.py
+++ project/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("postgresql://registration:r3gistr4tionp4ss@localhost/registrations", convert_unicode=True)
+engine = create_engine("postgresql://username:password@host/database", convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit = False, autoflush = False,
     bind=engine))
 Base = declarative_base()
